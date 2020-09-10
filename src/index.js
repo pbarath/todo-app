3ec252650543c206b0e9b92495417fe53ce61475
pbarath <barathpeti'gmail.com>
2020-09-10 22:10:17 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+//Component file
+import TodoContainer from "./components/TodoContainer"
+import "./App.css"
+
+ReactDOM.render(<TodoContainer />, document.getElementById("root"))
