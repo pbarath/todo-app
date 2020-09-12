b93227e5afc7df235a3a7614a13f6282564c81d6
pbarath <barathpeti'gmail.com>
2020-09-12 22:33:32 +0200

    Updates

---
+++ precache-manifest.00a52eb175763a133a26a4a62dd4429e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25485271ec5ebfffd18f63f64868e527",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "7b8352eb1c3b1117f83a",
+    "url": "/todo-app/static/css/main.461b0081.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/todo-app/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b8352eb1c3b1117f83a",
+    "url": "/todo-app/static/js/main.ddd98254.chunk.js"
+  },
+  {
+    "revision": "fcbffd53a1f01acb5e70",
+    "url": "/todo-app/static/js/runtime-main.9d2b6d0e.js"
+  }
+]);
