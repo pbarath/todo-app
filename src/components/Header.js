c12ce8bb388e55ddb7f4a16732efff2242347868
pbarath <barathpeti'gmail.com>
2020-09-13 17:41:02 +0200

    using-api

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,36 @@
-import React from "react"
+import React, { Component } from "react"
 
-const Header = () => {
+class Header extends Component {
+
+	componentDidUpdate(prevProps, prevState) {
+
+		var x = Math.floor(Math.random() *256);
+		var y = Math.floor(Math.random() *256);
+		var z = Math.floor(Math.random() *256);
+		var bgColor = "rgb(" + x + ", " + y +", " + z +")";
+
+		if (prevProps.headerSpan !== this.props.headerSpan) {
+			document.getElementById("inH1").innerHTML = "clicked";
+			document.getElementById("inH1").style.backgroundColor = bgColor;
+		}
+	}
+
+	render() {
 	const headerStyle = {
 		padding: "20px 0",
 		lineHeight: "2em",
 	}
 	return (
 		<header style={headerStyle}>
-			<h1 style={{ fontSize: "25px", marginBottom: "15px" }}>Simple Todo App</h1>
+			<h1 style={{ fontSize: "25px", marginBottom: "15px" }}>
+				Simple Todo App <span id="inH1"></span>
+			</h1>
 			<p style={{ fontSize: "19px" }}>
 				Please add todos item(s) through the input field
 			</p>
 		</header>
 		)
+	}
 }
 
 export default Header
--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -2,28 +2,14 @@ import React from "react"
 import TodoList from "./TodoList";
 import Header from "./Header"
 import InputTodo from "./InputTodo"
-import { v4 as uuidv4 } from  "uuid";
+//import { v4 as uuidv4 } from  "uuid";
+import axios from "axios";
 
 class TodoContainer extends React.Component {
 
 	state = {
-		todos: [
-		{
-			id: uuidv4(),
-			title: "Setup development environment",
-			completed: true
-		},
-		{
-			id: uuidv4(),
-			title: "Develop website and add content",
-			completed: true
-		},
-		{
-			id: uuidv4(),
-			title: "Deploy to live server",
-			completed: true
-		}
-		]
+		todos: [],
+		show: false
 	};
 
 	handleChange = (id) => {
@@ -33,35 +19,47 @@ class TodoContainer extends React.Component {
 					todo.completed = !todo.completed;
 				}
 				return todo;
-			})
-		});
+			}),
+			show: !this.state.show,
+		})
 	};
 
 	delTodo = id => {
-		this.setState({
-			todos: [
-				...this.state.todos.filter(todo => {
-					return todo.id !== id;
+		axios
+			.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+			.then( response =>
+				this.setState({
+					todos: [
+						...this.state.todos.filter(todo => {
+						return todo.id !== id;
+						}),
+					],
 				})
-			]
-		});
+			)
 	};
 
 	addTodoItem = title => {
-		const newTodo = {
-			id: uuidv4(),
-			title: title,
-			completed: false
-		};
-		this.setState({
-			todos: [...this.state.todos, newTodo]
-		});
+		axios
+			.post("https://jsonplaceholder.typicode.com/todos", {
+				title: title,
+				completed: false,
+			})
+			.then(response =>
+				this.setState({
+				todos: [...this.state.todos, response.data],
+				})
+			)
 	};
+
+	componentDidMount() {
+		axios.get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+		.then(response => this.setState({todos: response.data}));
+	}
 
 	render() {
 		return (
 			<div className="container">
-				<Header />
+				<Header headerSpan={this.state.show} />
 				<InputTodo addTodoProps={this.addTodoItem} />
 				<TodoList 
 					todos={this.state.todos} 
